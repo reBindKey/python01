a6a15f4907c5ec31a33a45dab726820d04237559
reBindKey <108424431+reBindKey@users.noreply.github.com>
2023-01-11 01:11:59 -0500

    Create blindToCase.py

---
+++ blindToCase.py
@@ -0,0 +1,14 @@
+# input comparison blind to capitalization
+
+input = 'trycodehere'
+
+keyword = 'TRYCODEHERE'
+
+if input.upper() == keyword:    #change here to make keyword insentitive to case
+    print('valid')
+else:
+    print('invalid')
+
+# trycodehere != TRYCODEHERE
+#
+
